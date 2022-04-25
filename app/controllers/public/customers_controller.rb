87776c3aa92dc69326f84aaef76b6b0a7fb3f4c7
yoshidashintarou <y.shintaro.1999@gmail.com>
2022-04-25 03:32:53 +0000

    [add admin/index]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,17 +1,17 @@
 class Public::CustomersController < ApplicationController
 
 def show
-
+ @customer = current_customer
 end
 
 def edit
-  @customer = Customer.find(params[:id])
+ @customer = current_customer
 end
 
  private
 
  def public_params
- params.require(:customer).permit(:sunname, :name, :sunname_kana, :name_kana, :postal_code, :address, :telephone_number)
+ params.require(:customer).permit(:sunname, :name, :sunname_kana, :name_kana, :postal_code, :address, :telephone_number, :email)
  end
 
 end
