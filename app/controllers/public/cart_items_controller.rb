e6aad3ed6a0ae1380714fc5dcb222b6a8a431298
a3232 <92038506+a3232@users.noreply.github.com>
2022-04-27 03:34:29 +0000

    カート機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,18 @@
 class Public::CartItemsController < ApplicationController
+
+  def index
+    @cart_items = CartItem.all
+  end
+
+  def create
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.save
+    redirect_to '/cart_items'
+  end
+
+  private
+  def cart_item_params
+    params.permit(:item_id, :amount)
+  end
+
 end
