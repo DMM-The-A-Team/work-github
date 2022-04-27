c34aa174a8de44be458b731fbfded8d768a7f488
yoshidashintarou <y.shintaro.1999@gmail.com>
2022-04-27 03:35:44 +0000

    [add 退会機能,細かい修正]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,12 +10,18 @@ end
 
 def update
  @customer = current_customer
+ if @customer.update(customer_params)
+  redirect_to customers_my_page_path
+ else
+ render :edit
+ end
 end
 
 
+ 
  private
 
- def public_params
+ def customer_params
  params.require(:customer).permit(:sunname, :name, :sunname_kana, :name_kana, :postal_code, :address, :telephone_number, :email)
  end
 
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -33,7 +34,10 @@ def customer_state
   return if !@customer
   ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
   if @customer.valid_password?(params[:customer][:password])
-    ## 【処理内容3】
+    ## 【処理内容3】退会していたらサインアップ画面に遷移する
+    if @customer.is_deleted == true
+      redirect_to  new_customer_registration_path
+    end
   end
 end
 end
